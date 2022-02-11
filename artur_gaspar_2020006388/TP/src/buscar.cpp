7ba8838306a0e3099abb03fa69a9a04d3c24f6e3
Artur <arthur.gaspar2011@gmail.com>
2022-02-11 10:37:16 -0300

    Adicionado modo de uso do programa.

--- artur_gaspar_2020006388/TP/src/buscar.cpp
+++ artur_gaspar_2020006388/TP/src/buscar.cpp
@@ -11,7 +11,7 @@
 #include "msgassert.hpp"
 
 // variaveis globais para opcoes
-std::string reg_nome;
+std::string reg_nome, querry_nome, out_nome, corpus_nome, stop_nome;
 int regmem;
 
 void uso()
@@ -19,6 +19,14 @@ void uso()
 // Entrada: nao tem
 // Saida: impressao das opcoes de linha de comando
 {
+    fprintf(stderr, "buscar\n");
+    fprintf(stderr, "\t-h      \t(obter ajuda) \n");
+    fprintf(stderr, "\t-i <arq>\t(arquivo de consulta) \n");
+    fprintf(stderr, "\t-o <arq>\t(arquivo de saida) \n");
+    fprintf(stderr, "\t-c <arq>\t(pasta com documentos - corpus) \n");
+    fprintf(stderr, "\t-s <arq>\t(arquivo de palavras a ignorar - stopwords) \n");
+    fprintf(stderr, "\t-p <arq>\t(arquivo de registro de desempenho)\n");
+    fprintf(stderr, "\t-l      \t(registrar padrao de acesso e localidade)\n");
 }
 
 void parse_args(int argc, char **argv)
