9b934021ac80ce14ccbb4a7292dd92b0c127277b
Artur <arthur.gaspar2011@gmail.com>
2022-02-11 10:39:38 -0300

    Adicionada leitura de argumentos da linha de comando.

--- artur_gaspar_2020006388/TP/src/buscar.cpp
+++ artur_gaspar_2020006388/TP/src/buscar.cpp
@@ -32,14 +32,87 @@ void uso()
 void parse_args(int argc, char **argv)
 // Descricao: le as opcoes da linha de comando e inicializa variaveis
 // Entrada: argc e argv
-// Saida: 
+// Saida: reg_nome, querry_nome, out_nome, corpus_nome, stop_nome, regmem
 {
+    // tratando caso de nao ter argumentos
+    if (argc == 1)
+    {
+        uso();
+        exit(1);
+    }
+
+    // variavel externa do getopt
+    extern char *optarg;
+
+    // variavel auxiliar
+    int c;
+
+    // inicializacao variaveis globais para opcoes
+
+    regmem = 0;
+    reg_nome[0] = 0;
+    querry_nome[0] = 0;
+    out_nome[0] = 0;
+    corpus_nome[0] = 0;
+    stop_nome[0] = 0;
+    bool i = false, o = false, c = false, s = false, p = false;
+
+    // getopt - letra indica a opcao, : junto a letra indica parametro
+    // no caso de escolher mais de uma operacao, vale a ultima
+    while ((c = getopt(argc, argv, "i:o:c:s:p:hl")) != EOF)
+    {
+        switch (c)
+        {
+        case 'i':
+            avisoAssert(!i, "Mais de um arquivo de consulta passado, o ultimo sera usado.");
+            i = true;
+            querry_nome = optarg;
+            break;
+        case 'o':
+            avisoAssert(!o, "Mais de um arquivo de saida passado, o ultimo sera usado.");
+            o = true;
+            out_nome = optarg;
+        case 'c':
+            avisoAssert(!c, "Mais de uma pasta de documentos passada, a ultima sera usada.");
+            c = true;
+            corpus_nome = optarg;
+            break;
+        case 's':
+            avisoAssert(!s, "Mais de um arquivo de stopwords passado, o ultimo sera usado.");
+            s = true;
+            stop_nome = optarg;
+            break;
+        case 'p':
+            avisoAssert(!p, "Mais de um arquivo de registro passado, o ultimo sera usado.");
+            p = true;
+            reg_nome = optarg;
+            break;
+        case 'l':
+            regmem = 1;
+            break;
+        case 'h':
+        default:
+            uso();
+            exit(1);
+        }
+    }
+    // verificacao da consistencia das opcoes
+    erroAssert(reg_nome.length() > 0,
+               "buscar - nome de arquivo de registro de acesso tem que ser definido.");
+    erroAssert(querry_nome.length() > 0,
+               "buscar - nome de arquivo de consulta tem que ser definido.");
+    erroAssert(out_nome.length() > 0,
+               "buscar - nome de arquivo de saida tem que ser definido.");
+    erroAssert(corpus_nome.length() > 0,
+               "buscar - nome da pasta de documentos tem que ser definido.");
+    erroAssert(stop_nome.length() > 0,
+               "buscar - nome de arquivo de stopwords tem que ser definido.");
 }
 
 int main(int argc, char **argv)
-// Descricao: programa principal para ordenacao externa de URLs
+// Descricao: programa principal para indexacao e busca de documentos
 // Entrada: argc e argv
-// Saida: no arquivo especificado escreve o resultado ordenado
+// Saida: no arquivo especificado escreve os 10 melhores documentos pra busca dada
 {
     // avaliar linha de comando
     parse_args(argc, argv);
