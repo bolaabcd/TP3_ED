4ec1f30898b60c79f4d7b418f0d0a1497bcc3c4d
Artur <arthur.gaspar2011@gmail.com>
2022-02-18 17:27:02 -0300

    Implementado arvore binaria (nao balanceada) de strings.

--- artur_gaspar_2020006388/TP/include/string_set.hpp
+++ artur_gaspar_2020006388/TP/include/string_set.hpp
@@ -1,22 +1,39 @@
 //---------------------------------------------------------------------
 // Arquivo      : string_set.hpp
-// Conteudo     : arquivo de cabecalho para set de stopwords.
+// Conteudo     : arquivo de cabecalho para set de strings.
 // Autor        : Artur Gaspar da Silva
 //---------------------------------------------------------------------
 
-#ifndef CONJUNTO_DE_STOPWORDS
-#define CONJUNTO_DE_STOPWORDS
+#ifndef CONJUNTO_DE_STRINGS
+#define CONJUNTO_DE_STRINGS
 
 #include <string>
 
+class String_No
+{
+public:
+    String_No(std::string str);
+    std::string str;
+    String_No *esq;
+    String_No *dir;
+    ~String_No();
+};
+
+// Implementado como arvore binaria
 class String_Set
 {
 public:
     String_Set();
-    void add(std::string stopword);
-    void remove(std::string stopword);
-    void is_in(std::string stopword);
+    void add(std::string str);
+    void add_auxiliar(String_No **raiz_atual, std::string);
+    bool is_in(std::string str);
+    bool is_in_auxiliar(String_No **raiz_atual, std::string str);
     int size();
+    ~String_Set();
+
+private:
+    String_No *raiz;
+    int tamanho;
 };
 
 #endif
--- artur_gaspar_2020006388/TP/src/string_set.cpp
+++ artur_gaspar_2020006388/TP/src/string_set.cpp
@@ -3,3 +3,83 @@
 // Conteudo     : arquivo de implementacao para set de stopwords.
 // Autor        : Artur Gaspar da Silva
 //---------------------------------------------------------------------
+
+#include "string_set.hpp"
+#include <string>
+
+String_No::String_No(std::string str)
+{
+    this->str = str;
+    this->esq = nullptr;
+    this->dir = nullptr;
+}
+
+String_No::~String_No()
+{
+    if (this->esq != nullptr)
+    {
+        this->esq->~String_No();
+        delete this->esq;
+    }
+    if (this->dir != nullptr)
+    {
+        this->dir->~String_No();
+        delete this->dir;
+    }
+}
+
+String_Set::String_Set()
+{
+    raiz = nullptr;
+    tamanho = 0;
+}
+
+void String_Set::add(std::string str)
+{
+    this->add_auxiliar(&(this->raiz), str);
+}
+
+void String_Set::add_auxiliar(String_No **raiz_atual, std::string str)
+{
+    if (*raiz_atual == nullptr)
+        *raiz_atual = new String_No(str);
+    else
+    {
+        if ((*raiz_atual)->str < str)
+            add_auxiliar(&((*raiz_atual)->dir), str);
+        else if ((*raiz_atual)->str > str)
+            add_auxiliar(&((*raiz_atual)->esq), str);
+    }
+}
+
+bool String_Set::is_in(std::string str)
+{
+    return this->is_in_auxiliar(&(this->raiz), str);
+}
+
+bool String_Set::is_in_auxiliar(String_No **raiz_atual, std::string str)
+{
+    if (*raiz_atual == nullptr)
+        return false;
+    else
+    {
+        if ((*raiz_atual)->str < str)
+            return is_in_auxiliar(&((*raiz_atual)->dir), str);
+        else if ((*raiz_atual)->str > str)
+            return is_in_auxiliar(&((*raiz_atual)->esq), str);
+    }
+}
+
+int String_Set::size()
+{
+    return this->tamanho;
+}
+
+String_Set::~String_Set()
+{
+    if (this->raiz != nullptr)
+    {
+        this->raiz->~String_No();
+        delete this->raiz;
+    }
+}
